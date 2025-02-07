02c18cebdd485fc39f8f2f1a56aa507f353b0b2c
AlysonRyan01 <sdhuahdsaih@gmail.com>
2025-02-07 17:33:28 -0300

    .

--- Loja.Web/Pages/ProdutoPage.razor.cs
+++ Loja.Web/Pages/ProdutoPage.razor.cs
@@ -2,6 +2,7 @@
 using Dima.Web.Security;
 using Loja.Core.Handlers;
 using Loja.Core.Models;
+using Loja.Core.Requisicoes.CarrinhoItens;
 using Loja.Core.Requisicoes.Produtos;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
@@ -16,6 +17,7 @@ public partial class ProdutoPageCode : ComponentBase
     public bool IsBusy { get; set; } = false;
     [Parameter]public long id { get; set; }
     public Produto Produto { get; set; }
+    public List<Produto> Produtos { get; set; }
     public bool _userLoggedIn { get; set; } = false;
     public ClaimsPrincipal _user { get; set; }
     public string ImagemPrincipal { get; set; } = string.Empty;
@@ -25,6 +27,7 @@ public partial class ProdutoPageCode : ComponentBase
     #region dependencies
     [Inject] public IJSRuntime JSRuntime { get; set; } = null!;
     [Inject] public IProdutoHandler handler { get; set; } = null!;
+    [Inject] public ICarrinhoItemHandler CarrinhoItemHandler { get; set; } = null!;
     [Inject] public NavigationManager NavigationManager { get; set; } = null!;
     [Inject] public ISnackbar Snackbar { get; set; } = null!;
     [Inject] public ICookieAuthenticationStateProvider AuthenticationState { get; set; }
@@ -80,9 +83,48 @@ public partial class ProdutoPageCode : ComponentBase
         {
             Snackbar.Add("Erro ao carregar produto...", Severity.Error);
         }
+
+        try
+        {
+            var result = await handler.ObterTodosProdutos();
+            if (result.IsSuccess)
+            {
+                Produtos = result.Dados ?? new List<Produto>();
+            }
+        }
+        catch 
+        {
+            Snackbar.Add("Erro ao carregar os produtos recomendados...", Severity.Error);
+        }
         finally
         {
             IsBusy = false;
+        }
+    }
+    
+    protected override async Task OnParametersSetAsync()
+    {
+        try
+        {
+            var requisicao = new ObterProdutoPorIdRequisicao
+            {
+                Id = id
+            };
+
+            var result = await handler.ObterProdutoPorIdAsync(requisicao);
+            if (result.IsSuccess)
+            {
+                Produto = result.Dados;
+                ImagemPrincipal = $"{WebConfiguration.BackendUrl}{Produto.Imagens.First().Url}";
+            }
+            else
+            {
+                Snackbar.Add("Falha ao carregar as informações do produto...", Severity.Error);
+            }
+        }
+        catch
+        {
+            Snackbar.Add("Erro ao carregar a pagina...", Severity.Error);
         }
     }
 
@@ -93,4 +135,34 @@ public partial class ProdutoPageCode : ComponentBase
         await JSRuntime.InvokeVoidAsync("changeMainImage", imageUrl);
     }
     
+    public async Task AdicionarAoCarrinho(long produtoId)
+    {
+        try
+        {
+            var request = new CriarCarrinhoItemRequisicao
+            {
+                ProdutoId = produtoId,
+                Quantidade = 1
+            };
+
+            if (_userLoggedIn == false)
+            {
+                Snackbar.Add("Você precisa fazer login para continuar", Severity.Info);
+                NavigationManager.NavigateTo("/entrar");
+                return;
+            }
+                
+            var result = await CarrinhoItemHandler.CriarCarrinhoItemAsync(request, _user);
+            if(result.IsSuccess)
+                Snackbar.Add("Produto adicionado ao carrinho!", Severity.Success);
+            else
+            {
+                Snackbar.Add(result.Mensagem ?? "Erro ao adicionar o produto ao carrinho", Severity.Error);
+            }
+        }
+        catch (Exception e)
+        {
+            Snackbar.Add("Erro ao adicionar o produto ao carrinho", Severity.Error);
+        }
+    }
 }

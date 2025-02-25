ce6a9da3c61942459a20f37fb0baafe4ca39f3ea
Alyson Ryan Ullirsch <sdhuahdsaih@gmail.com>
2025-02-25 00:26:08 -0300

    .

---
+++ Loja.Core/Requisicoes/Contato/ContatoRequisicao.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Loja.Core.Requisicoes.Contato;
+
+public class ContatoRequisicao
+{
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O e-mail é obrigatório.")]
+    [EmailAddress(ErrorMessage = "E-mail inválido.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "A mensagem é obrigatória.")]
+    public string Mensagem { get; set; }
+}
--- Loja.Web/Pages/HomePages/Home.razor.cs
+++ Loja.Web/Pages/HomePages/Home.razor.cs
@@ -1,8 +1,30 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Loja.Core.Requisicoes.Contato;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
 
 namespace Dima.Web.Pages.HomePages;
 
 public partial class HomePage : ComponentBase
 {
     
+    public ContatoRequisicao contato = new ContatoRequisicao();
+    public bool valid;
+    
+    [Inject] public IJSRuntime JSRuntime { get; set; } = null!;
+    [Inject] public ISnackbar Snackbar { get; set; } = null!;
+    
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("initAOS");
+        }
+    }
+    
+    public void EnviarFormulario()
+    {
+        Console.WriteLine($"Nome: {contato.Nome}, E-mail: {contato.Email}, Mensagem: {contato.Mensagem}");
+        Snackbar.Add("Mensagem enviada com sucesso!", Severity.Success);
+    }
 }
